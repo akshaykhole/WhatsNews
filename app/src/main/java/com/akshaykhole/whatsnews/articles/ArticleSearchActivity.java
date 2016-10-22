48c5e0653795e6168650954f4eca93a4125c9779
Akshay Khole <akshay@reversehack.in>
2016-10-22 16:20:15 -0700

    Adds search bar to action bar

--- app/src/main/java/com/akshaykhole/whatsnews/articles/ArticleSearchActivity.java
+++ app/src/main/java/com/akshaykhole/whatsnews/articles/ArticleSearchActivity.java
@@ -1,8 +1,12 @@
 package com.akshaykhole.whatsnews.articles;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,9 +28,7 @@ import butterknife.OnClick;
 import cz.msebera.android.httpclient.Header;
 
 public class ArticleSearchActivity extends AppCompatActivity {
-    @BindView(R.id.btnSearch) Button btnSearch;
     @BindView(R.id.gvArticles) GridView gvArticles;
-    @BindView(R.id.searchQuery) EditText searchQuery;
 
     private ArrayList<ArticlesModel> articles;
     ArticleArrayAdapter adapter;
@@ -40,32 +42,51 @@ public class ArticleSearchActivity extends AppCompatActivity {
         initialize();
     }
 
-    @OnClick
-    public void onArticleSearch(View view) {
-        adapter.clear();
-        String query = searchQuery.getText().toString();
-        ArticleSearchClient articleFetcher = new ArticleSearchClient();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.article_search_action_bar, menu);
 
-        articleFetcher.search(query, 0, new JsonHttpResponseHandler() {
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
 
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchMenuItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                try {
-                    JSONArray articlesResponse = response.getJSONObject("response").getJSONArray("docs");
-                    articles.addAll(ArticlesModel.fromJSONArray(articlesResponse));
-//                    Log.d("DEBUG", articles.toString());
-                    adapter.notifyDataSetChanged();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            public boolean onQueryTextSubmit(String query) {
+                adapter.clear();
+                ArticleSearchClient articleFetcher = new ArticleSearchClient();
+
+                articleFetcher.search(query, 0, new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        try {
+                            JSONArray articlesResponse = response.getJSONObject("response").getJSONArray("docs");
+                            articles.addAll(ArticlesModel.fromJSONArray(articlesResponse));
+                            Log.d("DEBUG", articles.toString());
+                            adapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        showToast("Oops! Something went wrong. We are trying hard to fix it!");
+                        Log.d("DEBUG", responseString);
+                    }
+                });
+
+                searchView.clearFocus();
+                return true;
             }
 
             @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                showToast("Oops! Something went wrong. We are trying hard to fix it!");
-                Log.d("DEBUG", responseString);
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
+
+        return true;
     }
 
     public void showToast(String text) {
