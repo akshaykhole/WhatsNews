0892e026a0750b249d01e1167c2555a888cc8055
Akshay Khole <akshay@reversehack.in>
2016-10-23 15:19:00 -0700

    * Check for network and Internet connectivity

--- app/src/main/java/com/akshaykhole/whatsnews/articles/ArticleSearchActivity.java
+++ app/src/main/java/com/akshaykhole/whatsnews/articles/ArticleSearchActivity.java
@@ -1,6 +1,9 @@
 package com.akshaykhole.whatsnews.articles;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +24,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -38,6 +42,7 @@ public class ArticleSearchActivity extends AppCompatActivity {
     private static SearchView searchView;
     private static final String oopsString = "Oops! Something went wrong. We are trying hard to fix it!";
     private static Intent intentFromFilter;
+    private static final String googleDNSAddress = "8.8.8.8";
     ArticleArrayAdapter adapter;
 
     @Override
@@ -201,8 +206,13 @@ public class ArticleSearchActivity extends AppCompatActivity {
         searchQuery = null;
         params.put("api-key", ArticleSearchClient.articleApiKey);
         params.put("page", page);
-        setParamsForSearch();
-        setSearchResults();
+
+        if(isNetworkAvailable() && isOnline()) {
+            setParamsForSearch();
+            setSearchResults();
+        } else {
+            showToast("Please ensure you are connected to the Internet and try again!");
+        }
 
         // Click listener on GV
         gvArticles.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -214,5 +224,23 @@ public class ArticleSearchActivity extends AppCompatActivity {
                 startActivity(webviewIntent);
             }
         });
+    }
+
+    private Boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+
+    public boolean isOnline() {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 " + googleDNSAddress);
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        } catch (IOException e)          { e.printStackTrace(); }
+        catch (InterruptedException e) { e.printStackTrace(); }
+        return false;
     }
 }
